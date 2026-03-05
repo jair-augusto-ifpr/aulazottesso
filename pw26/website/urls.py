f2e8732e8b3e7a545c6c111948dd189fdf165ada
jair-augusto-ifpr <20233012578@estudantes.ifpr.edu.br>
2026-03-05 09:18:08 -0300

    criando alguns templates e makefile

--- pw26/website/urls.py
+++ pw26/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import Index, Sobre, Contato
 
 urlpatterns = [
-    path("home/", Index.as_view(), name="home"),
+    path("home", Index.as_view(), name="home"),
+    path("sobre", Sobre.as_view(), name="sobre"),
+    path("contato", Contato.as_view(), name="contato"),
 ]
--- pw26/website/views.py
+++ pw26/website/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "website/index.html"
+
+class Sobre(TemplateView):
+    template_name = "website/sobre.html"
+
+class Contato(TemplateView):
+    template_name = "website/contato.html"
